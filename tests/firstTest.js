ebccb82c501743dc940a1d4efc0f0d9995d1664e
gcarlin01 <giancarlo.carlin01@gmail.com>
2024-03-12 16:47:26 -0400

    adds first test

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -8,7 +8,11 @@ async function example() {
 
   let driver = await new Builder().forBrowser("firefox").build();
   await driver.get("https://lambdatest.github.io/sample-todo-app/");
-  await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
-  await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
+  await driver
+    .findElement({ id: "sampletodotext" })
+    .sendKeys("Learn to automate with Selenium");
+  await driver.findElement({ id: "addbutton" }).click();
   await driver.quit();
 }
+
+example();
