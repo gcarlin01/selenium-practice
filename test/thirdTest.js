9c05fa8d0f9143192e3b6700aa82fdcb6b6959de
gcarlin01 <giancarlo.carlin01@gmail.com>
2024-03-12 19:58:20 -0400

    adds mochawesome for reporting

---
+++ test/thirdTest.js
@@ -0,0 +1,30 @@
+const { Builder } = require("selenium-webdriver");
+var should = require("chai").should();
+
+describe("add another todo app test", function () {
+  it("successfully adds another todo to application", async function () {
+    // launch the browser
+    let driver = await new Builder().forBrowser("firefox").build();
+
+    // navigate to the url
+    await driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+    // add a todo using locators and actions
+    await driver
+      .findElement({ id: "sampletodotext" })
+      .sendKeys("Learn to automate with Selenium");
+    await driver.findElement({ id: "addbutton" }).click();
+
+    // assert the todo
+    let newTodo = await driver
+      .findElement({
+        xpath: "//li[last()]",
+      })
+      .getText();
+
+    // assert using chai should library
+    newTodo.should.equal("Learn to automate with Selenium");
+
+    await driver.quit();
+  });
+});
